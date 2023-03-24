c9379ae14d7872561a8c2040ef67ec0d16b24de3
formosoantoine <81078336+formosoantoine@users.noreply.github.com>
2023-03-24 15:21:20 +0100

    add toast management

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Button from "../Button";
 import VariantToast from "../VariantToast";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -10,6 +11,12 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [popToast, setPopToast] = React.useState(false);
+
+  function submitToast(event) {
+    event.preventDefault();
+    setPopToast(true);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -18,42 +25,52 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
+      {popToast && (
+        <Toast
+          message={message}
+          type={variant}
+          handleClose={() => setPopToast(false)}
+        />
+      )}
+
       <div className={styles.controlsWrapper}>
-        <div className={styles.row}>
-          <label
-            htmlFor="message"
-            className={styles.label}
-            style={{ alignSelf: "baseline" }}
-          >
-            Message
-          </label>
-          <div className={styles.inputWrapper}>
-            <textarea
-              id="message"
-              className={styles.messageInput}
-              onChange={(event) => setMessage(event.target.value)}
-              value={message}
-            />
+        <form onSubmit={submitToast}>
+          <div className={styles.row}>
+            <label
+              htmlFor="message"
+              className={styles.label}
+              style={{ alignSelf: "baseline" }}
+            >
+              Message
+            </label>
+            <div className={styles.inputWrapper}>
+              <textarea
+                id="message"
+                className={styles.messageInput}
+                onChange={(event) => setMessage(event.target.value)}
+                value={message}
+              />
+            </div>
           </div>
-        </div>
 
-        <div className={styles.row}>
-          <div className={styles.label}>Variant</div>
-          <div className={`${styles.inputWrapper}`}>
-            <VariantToast
-              variants={VARIANT_OPTIONS}
-              checked={variant}
-              handleCheckChange={setVariant}
-            />
+          <div className={styles.row}>
+            <div className={styles.label}>Variant</div>
+            <div className={`${styles.inputWrapper}`}>
+              <VariantToast
+                variants={VARIANT_OPTIONS}
+                checked={variant}
+                handleCheckChange={setVariant}
+              />
+            </div>
           </div>
-        </div>
 
-        <div className={styles.row}>
-          <div className={styles.label} />
-          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+          <div className={styles.row}>
+            <div className={styles.label} />
+            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+              <Button>Pop Toast!</Button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
